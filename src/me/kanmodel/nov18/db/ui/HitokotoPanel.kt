5b1e33d74efa459067c1d03181f745075941a29e
KanModel <136995219@qq.com>
2018-11-29 14:04:25 +0800

    首页添加图片

--- src/me/kanmodel/nov18/db/ui/HitokotoPanel.kt
+++ src/me/kanmodel/nov18/db/ui/HitokotoPanel.kt
@@ -4,14 +4,20 @@ import me.kanmodel.nov18.db.database.DataQuery
 import java.awt.Dimension
 import java.awt.Font
 import java.awt.GridLayout
+import java.awt.Image
 import java.awt.event.ActionListener
 import javax.swing.*
 import java.awt.event.ItemEvent
 
 internal class HitokotoPanel : JPanel(GridLayout(1, 1)) {
-    private val randomTextPane = JTextPane()
+    private val randomTextPane = RandomTextPane()
 
     init {
+        for (i in 0..1) {
+            imageIcons.add(ImageIcon("image\\" + i.toString() + ".jpg"))
+            imageIcons[i].image = imageIcons[i].image.getScaledInstance(1000, 800, Image.SCALE_DEFAULT)
+        }
+
         val typeList = arrayOf("所有", "动画", "漫画", "游戏", "小说", "原创", "来自网络", "其他")
         val searchBox = JComboBox(typeList)
         searchBox.maximumSize = Dimension(80, 20)
@@ -33,9 +39,17 @@ internal class HitokotoPanel : JPanel(GridLayout(1, 1)) {
                         "\n\r                             -「${hitokoto.from}」"
             }
         }).start()
+
+        Timer(randomImageInterval, ActionListener {
+            indexIcon++
+        }).start()
     }
 
     companion object {
         var randomInterval = 2000
+        var randomImageInterval = 10000
+        var indexIcon = 1
+        val imageIcons = ArrayList<ImageIcon>()
+
     }
 }
---
+++ src/me/kanmodel/nov18/db/ui/RandomTextPane.kt
@@ -0,0 +1,38 @@
+package me.kanmodel.nov18.db.ui
+
+import java.awt.Color
+import java.awt.Graphics
+import javax.swing.ImageIcon
+import javax.swing.JTextPane
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: KanModel
+ * Date: 2018-11-29-13:58
+ */
+/**
+ * @description: todo
+ * @author: KanModel
+ * @create: 2018-11-29 13:58
+ */
+class RandomTextPane : JTextPane() {
+    init {
+        isOpaque = false
+        // this is needed if using Nimbus L&F - see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6687960
+        background = Color(0, 0, 0, 0)
+    }
+
+    override fun paintComponent(g: Graphics) {
+        // set background green - but can draw image here too
+        //            g.setColor(Color.GREEN);
+        //            g.fillRect(0, 0, getWidth(), getHeight());
+
+        // uncomment the following to draw an image
+//        val imageIcon = ImageIcon("image\\ADA.png")
+        val img = HitokotoPanel.imageIcons[HitokotoPanel.indexIcon % 2].image
+        g.drawImage(img, 0, 0, this)
+
+        super.paintComponent(g)
+    }
+}
